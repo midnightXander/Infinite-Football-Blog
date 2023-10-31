66e1ed3d7fbb5aed86135bb2c560c86c4a99421a
NgaikamAlex <denzelwashington913@gmail.com>
2023-10-31 09:43:19 +0100

    adjusted index

--- core/views.py
+++ core/views.py
@@ -26,7 +26,7 @@ def post(request,category_name,post_id):
 
 def category(request,category_name):
     """posts from a specific category"""
-    category = get_object_or_404(Category, name=category_name)
+    category = Category.objects.get(name=category_name)
     posts = category.blogpost_set.order_by('-date_added')
 
     context = {"posts":posts,"category":category}
