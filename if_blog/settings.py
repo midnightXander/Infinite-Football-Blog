e65353e7a53468539d7e4f8972fee07fef9b7e85
NgaikamAlex <denzelwashington913@gmail.com>
2024-03-05 12:02:21 +0100

    removed heroku

--- if_blog/settings.py
+++ if_blog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+#import django_heroku
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,4 +149,11 @@ mimetypes.add_type("text/css", ".css", True)
 mimetypes.add_type("text/javascript",".js",True)
 
 #Heroku settings
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
+#django-heroku==0.3.1
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
